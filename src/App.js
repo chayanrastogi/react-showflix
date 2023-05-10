affc697cd7897c8c141400bfee9f206a6791d342
Chayan Rastogi <cp.chayanrastogi@gmail.com>
2023-05-11 02:05:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ShowList from './components/ShowList';
 import ShowSummary from './components/ShowSummary';
 import Booking from './components/Booking';
